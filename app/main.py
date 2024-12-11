c5b75152648872ea684a4229e32fdf7846e2afdd
Oleg Korol <oleg@olegkorol.de>
2024-12-11 21:56:26 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -17,21 +17,23 @@ def main():
     with open(filename) as file:
         file_contents = file.read()
 
-    # Uncomment this block to pass the first stage
     if file_contents:
         for char in file_contents:
-            if char in ["(", ")"]:
-                scan_paranthesis(char)
+            scanner(char)
         sys.stdout.write("EOF  null\n")
     else:
         print("EOF  null") # Placeholder, remove this line when implementing the scanner
 
-def scan_paranthesis(char):
+def scanner(char):
     match char:
         case "(":
             sys.stdout.write("LEFT_PAREN ( null\n")
         case ")":
             sys.stdout.write("RIGHT_PAREN ) null\n")
+        case "{":
+            sys.stdout.write("LEFT_BRACE { null\n")
+        case "}":
+            sys.stdout.write("RIGHT_BRACE } null\n")
 
 
 if __name__ == "__main__":
